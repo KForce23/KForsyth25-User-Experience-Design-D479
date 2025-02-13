3b2867e0687348338dfa857da0cd396eb0ff2bd9
KForce23 <123131083+KForce23@users.noreply.github.com>
2025-02-12 20:22:02 -0800

    Launch 1.0
    First commit 2.12.25

---
+++ js_files/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Website Loaded Successfully!");
+});
+
+/* TODO */
+/* create login */
+/* create bookNow */
+/* create searchfield */
+/* create searchButton */
